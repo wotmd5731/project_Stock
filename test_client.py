d922f57ba92476b88a4fe234efdf9d01a08e154d
jaeshan <wotmd5731@naver.com>
2019-05-15 21:11:25 +0900

    add

--- test_client.py
+++ test_client.py
@@ -3,15 +3,35 @@
 import socket
 import json
 
+def fa(aa):
+    print('fa:',aa)
+    return 'fa:aa'
+
+def fb(aa):
+    print('fb:',aa)
+    return 'fb:abba'
+
+def remote_inst(s,fname,farg):
+    data = json.dumps({'name':fname,'arg':farg})
+    s.send(bytes(data,'utf-8'))
+    data = s.recv(1024)
+    recvdata = json.loads(data.decode('utf-8'))
+    return recvdata
+
+def remote_call(s):
+    data = s.recv(1024)
+    print(data)
+    if not data:return -1
+    recvdata = json.loads(data.decode('utf-8'))
+    ret = eval(recvdata['name']+'('+str(recvdata['arg'])+')')
+    data = json.dumps([ret])
+    s.send(bytes(data,'utf-8'))
+
 
 HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    dd = json.dumps({'a':[1,23,3,4,5],'b':"123213"})
-    
-    
-    s.send(bytes(dd,"utf-8"))
-    
-    data = s.recv(1024)
-print('Received', repr(data))
+    ret = remote_inst(s,'fa',[1])
+    print(ret)
+
--- test_server.py
+++ test_server.py
@@ -6,6 +6,33 @@ import json
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
+
+def fa(aa):
+    print('fa:',aa)
+    return 'fa:aa'
+
+def fb(aa):
+    print('fb:',aa)
+    return 'fb:abba'
+
+def remote_inst(s,fname,farg):
+    data = json.dumps({'name':fname,'arg':farg})
+    s.send(bytes(data,'utf-8'))
+    data = s.recv(1024)
+    recvdata = json.loads(data.decode('utf-8'))
+    return recvdata
+
+def remote_call(s):
+    data = s.recv(1024)
+    print(data)
+    if not data:return -1
+    recvdata = json.loads(data.decode('utf-8'))
+    ret = eval(recvdata['name']+'('+str(recvdata['arg'])+')')
+    data = json.dumps([ret])
+    s.send(bytes(data,'utf-8'))
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
@@ -14,12 +41,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         while True:
             print('a')
-            data = conn.recv(2048)
-            aa = json.loads(data)
-            print(aa['a'])
-            print(aa['b'])
-            if not data: 
-                print('brake')
+            ret = remote_call(conn)
+            if ret == -1:
                 break
-            conn.sendall(b'a')
-                
